4bdb94870cc8c6c624666d5d7f7c255fdc2ce9ef
Noor23 <noor23@gmail.com>
2023-03-16 11:24:48 +0300

    added homepage, Channel Model, User Model @alinabeel23 @nooralhajri @yusufaqeel

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    joindate = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    #foreign key of the user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
