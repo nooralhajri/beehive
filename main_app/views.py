eb22a0d805f09c571b944d309e7a30cecc33257d
baderrabeea <baderjamal4@gmail.com>
2023-03-16 15:25:42 +0300

    adding the sign up page and function @nooralhajri @alinabeel23 @yusufaqeel @bader973

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Channel
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
@@ -7,3 +7,17 @@ from django.contrib.auth import login
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            error_message = 'hey stupid try Again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_massage': error_message}
+    return render(request, 'registration/signup.html', context)
