9aef66b79bb6326c4fe45ae7e3e45e337e1fc6e0
Noor23 <noor23@gmail.com>
2023-03-16 13:51:15 +0300

    added signin, login, logout  @nooralhajri @alinabeel23 @yusufaqeel @bader973

--- beehive/settings.py
+++ beehive/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # specify where successful logins should redirect
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = '/'
 
 # specify where logout should redirect
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
