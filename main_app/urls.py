e9a7fc1c326b78c8ecaecf6451e948786c816dc8
Ali Nabeee <alinabeel.lab@gmail.com>
2023-03-16 16:06:13 +0300

    added crud operations for video and channel models @nooralhajri @alinabeel23 @yusufaqeel @bader973

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,16 @@ from. import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup', views.signup, name='signup'),
+
+    path('videos/', views.VideoList.as_view(), name='videos_index'),
+    path('videos/<int:pk>', views.VideoDetail.as_view(), name='videos_detail'),
+    path('videos/create', views.VideoCreate.as_view(), name='videos_create'),
+    path('videos/update/<int:pk>', views.VideoUpdate.as_view(), name='videos_update'),
+    path('videos/delete/<int:pk>', views.VideoDelete.as_view(), name='videos_delete'),
+
+    path('channels/', views.ChannelList.as_view(), name='channels_index'),
+    path('channels/<int:pk>', views.ChannelDetail.as_view(), name='channels_detail'),
+    path('channels/create', views.ChannelCreate.as_view(), name='channels_create'),
+    path('channels/update/<int:pk>', views.ChannelUpdate.as_view(), name='channels_update'),
+    path('channels/delete/<int:pk>', views.ChannelDelete.as_view(), name='channels_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Channel
+from .models import Channel, Video
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 
@@ -21,3 +24,55 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_massage': error_message}
     return render(request, 'registration/signup.html', context)
+
+
+
+# VIDEO CLASS BASED VIEWS
+class VideoList(ListView):
+    model = Video
+    
+
+class VideoCreate(CreateView):
+    model = Video
+    fields = '__all__'
+    
+    
+class VideoDetail(DetailView):
+    model = Video
+
+
+class VideoUpdate(UpdateView):
+    model = Video
+    fields = '__all__'
+    
+    
+class VideoDelete(DeleteView):
+    model = Video
+    success_url = '/'
+
+
+# CHANNEL CLASS BASED VIEWS
+class ChannelList(ListView):
+    model = Channel
+    
+
+class ChannelCreate(CreateView):
+    model = Channel
+    fields = '__all__'
+    
+    
+class ChannelDetail(DetailView):
+    model = Channel
+
+
+class ChannelUpdate(UpdateView):
+    model = Channel
+    fields = '__all__'
+    
+    
+class ChannelDelete(DeleteView):
+    model = Channel
+    success_url = '/'
+
+
+
