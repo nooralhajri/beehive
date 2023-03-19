dd44e812dad0ba59aaef56b5e251cbc56129c721
Noor23 <noor23@gmail.com>
2023-03-19 10:41:19 +0300

    deleted redundant uploaded images and video and extra urls @nooralhajri @alinabeel23 @yusufaqeel @bader973

--- beehive/urls.py
+++ beehive/urls.py
@@ -6,15 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', include('main_app.urls')),
-
-    path('accounts/change_password/', include('main_app.urls')),
-    path('accounts/change_password_done/', include('main_app.urls')),
-
-    path('accounts/password_reset/', include('main_app.urls')),
-    path('accounts/password_reset_done/', include('main_app.urls')),
-    path('accounts/password_reset_confirm/', include('main_app.urls')),
-    path('accounts/password_reset_complete/', include('main_app.urls')),
 ]
-
-
