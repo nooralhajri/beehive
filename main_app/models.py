ccc033c80c1a7aa3c1b2aa4af153e49ca0b31cdf
Ali Nabeee <alinabeel.lab@gmail.com>
2023-03-19 12:17:41 +0300

    added footer

--- main_app/models.py
+++ main_app/models.py
@@ -5,14 +5,7 @@ from django.core.validators import FileExtensionValidator
 
 
 # Create your models here.
-class Video(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    thumbnail = models.ImageField(upload_to='main_app/static/uploads/', default='')
-    video = models.FileField(upload_to='main_app/static/uploads/', null=True, validators=[FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])])
 
-    def get_absolute_url(self):
-        return reverse('videos_detail', kwargs={'pk': self.id})
     
 
 class Channel(models.Model):
@@ -21,6 +14,15 @@ class Channel(models.Model):
     profilephoto = models.ImageField(upload_to='main_app/static/uploads/', blank=True)
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE, null=True)
 
 
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    thumbnail = models.ImageField(upload_to='main_app/static/uploads/', default='')
+    video = models.FileField(upload_to='main_app/static/uploads/', null=True, validators=[FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])])
+
+    def get_absolute_url(self):
+        return reverse('videos_detail', kwargs={'pk': self.id})
+
+    
